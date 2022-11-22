e083a5449506274c9474dd655d4bf8a9f66d1694
Maxime Lebrun <maxime.lebrun.01@edu.cegeptr.qc.ca>
2022-11-22 08:57:37 -0500

    test BD

---
+++ Covoiturage/GestionBD2.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.UI.Xaml.Media.Animation;
+
+namespace Covoiturage
+{
+    internal class GestionBD2
+    {
+        MySqlConnection con;
+        ObservableCollection<Maison> liste;
+
+        static GestionBD gestionBD = null;
+
+        public GestionBD()
+        {
+            con = new MySqlConnection("Server=localhost;Database=prog;Uid=root;Pwd=root;");
+            liste = new ObservableCollection<Maison>();
+        }
+
+        public static GestionBD getInstance()
+        {
+            if (gestionBD == null)
+                gestionBD = new GestionBD();
+
+            return gestionBD;
+        }
+    }
+}
