100a1e9929e5fc67398b284bfcd7ca5d08cc36da
Maxime Lebrun <maxime.lebrun.01@edu.cegeptr.qc.ca>
2022-12-01 14:47:32 -0500

    debut histo

--- Covoiturage/ShowHistorique.xaml.cs
+++ Covoiturage/ShowHistorique.xaml.cs
@@ -1,7 +1,4 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License. See LICENSE in the project root for license information.
-
-using Microsoft.UI.Xaml;
+﻿using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
 using Microsoft.UI.Xaml.Data;
@@ -24,17 +21,11 @@ namespace Covoiturage
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class Historique : Page
+    public sealed partial class ShowHistorique : Page
     {
-        public Historique()
+        public ShowHistorique()
         {
             this.InitializeComponent();
-            histoListe.ItemsSource = GestionBD.getInstance().GetListeTrajet();
-        }
-
-        private void trajetListe_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
         }
     }
 }
